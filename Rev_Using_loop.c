c08b8f3491f6141e75cd0960eb64b2b233a24d02
Rajan Chaurasiya <chaurasiyarajan317@gmail.com>
2026-03-21 16:36:40 +0530

    REV_USING_LOOP

---
+++ Rev_Using_loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int n,rev=0;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    while(n>0){
+        rev = rev*10 + n%10;
+        n = n/10;
+    }
+    printf("Reverse of given no. is = %d ",rev);
+return 0;
+}
